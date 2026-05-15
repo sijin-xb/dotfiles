138db1ff702a14aef88a317f43ea5541da1a2b84
xibie <xbie96750@gmail.com>
2026-05-15 21:37:51 +0800

    Update dotfiles: 2026-05-15 21:37:51

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -3,3 +3,9 @@ require("config.lazy")
 
 -- 保存文件：Ctrl + s
 vim.keymap.set({ "n", "i", "v" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
+
+-- 复制：在可视化模式下按 Ctrl + C 复制到系统剪贴板
+vim.keymap.set("v", "<C-c>", '"+y', { desc = "Copy to clipboard" })
+
+-- 粘贴：在普通模式和插入模式下按 Ctrl + V 粘贴系统剪贴板内容
+vim.keymap.set({ "n", "i" }, "<C-v>", "<C-r>+", { desc = "Paste from clipboard" })
